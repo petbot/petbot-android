7f41871ad14928008f6082a51b9197ba87fba286
Misko <mouse9911@gmail.com>
2014-07-05 21:08:05 -0400

    fix to build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 if [ -z "$ANDROID_SDK" ]; then
 	echo Please set ANDROID_SDK correctly
 fi
-if [ -n "$ANDROID_NDK" ]; then
+if [ -z "$ANDROID_NDK" ]; then
 	echo Please set ANDROID_NDK correctly
 fi
 
-export PATH=$PATH:${ANDROID_SDK} 
+export PATH=$PATH:${ANDROID_NDK} 
 
 git submodule update --init
 cd ijkplayer
 ./init-android.sh
 cd android
-./compile-ffmpeg.sh
+./compile-ffmpeg.sh all
 ./compile-ijk.sh

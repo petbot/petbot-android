503aeffd0cd54152bbff7f37083d397450a25e23
Misko <mouse9911@gmail.com>
2014-07-05 21:33:58 -0400

    fix for build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 if [ -z "$ANDROID_SDK" ]; then
 	echo Please set ANDROID_SDK correctly
+	exit
 fi
 if [ -z "$ANDROID_NDK" ]; then
 	echo Please set ANDROID_NDK correctly
+	exit
 fi
 
 export PATH=$PATH:${ANDROID_NDK} 

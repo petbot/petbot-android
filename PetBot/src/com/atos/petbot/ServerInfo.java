82ee2f2b16eeb15cd4f42f5b7daf508b68834f72
ssitwell <ssitwell@gmail.com>
2014-07-13 23:14:58 -0400

    Have server url point to production server.

--- PetBot/src/com/atos/petbot/ServerInfo.java
+++ PetBot/src/com/atos/petbot/ServerInfo.java
@@ -4,5 +4,5 @@ package com.atos.petbot;
  * Created by root on 6/25/14.
  */
 public class ServerInfo {
-	public final static String url = "http://www.petbot.ca:5100";
+	public final static String url = "https://www.petbot.ca";
 }
